b8f5515b38d2228eaa53cd6063d860a6f549600a
Kelly Miadzieles <kellymiadzieles@gmail.com>
2021-05-05 13:50:59 +0300

    marketplace uus_kelly

--- src/main.js
+++ src/main.js
@@ -3,10 +3,7 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import Dropdown from 'vue-simple-search-dropdown'
 import vuetify from './plugins/vuetify'
-
-Vue.use(Dropdown);
 
 Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
--- src/views/Marketplace.vue
+++ src/views/Marketplace.vue
@@ -1,61 +1,45 @@
 <template>
   <div class="home">
-    <h1>Marketplace</h1>
-    <br>
-    <h3>Serach for an advertisement</h3>
-    <br>
-    <h4>Category</h4>
-    <input v-model="category1" placeholder="Choose category"/>
-    <br>
-    <h4>Price</h4>
-    <input v-model="price1" placeholder="Price from"/>
-    <input v-model="price1" placeholder="Price to"/>
-    <br>
-
-
-    <h4>Location</h4>
-    <input v-model="location1" placeholder="Choose location"/>
-
-
-<!--    <Dropdown-->
-<!--        :options="[{ id: 1, name: 'Harju maakond'}, { id: 2, name: 'Hiiu maakond'}]"-->
-<!--        v-on:selected="location1"-->
-<!--        v-on:filter="getAdsByLocation"-->
-<!--        :disabled="false"-->
-<!--        name="location"-->
-<!--        :maxItem="2"-->
-<!--        placeholder="Please select an option">-->
-<!--      {{getAds}}-->
-<!--    </Dropdown>-->
-
-
-<!--    <select v-model="location1" name="location">-->
-<!--      <button v-on:click="getAdsByLocation()">Enter</button>-->
-<!--      {{getAds}}-->
-
-
-
-<!--      <option value="Harju maakond">Harju maakond</option>-->
-<!--      <option value="Hiiu maakond">Hiiu maakond</option>-->
-<!--      <option value="Ida-Viru maakond">Ida-Viru maakond</option>-->nu
-<!--      <option value="Jõgeva maakond">Jõgeva maakond</option>-->
-<!--      <option value="Järva maakond">Järva maakond</option>-->
-<!--      <option value="Lääne maakond">Lääne maakond</option>-->
-<!--      <option value="Lääne-Viru maakond">Lääne-Viru maakond</option>-->
-<!--      <option value="Ida-Viru maakond">Ida-Viru maakond</option>-->
-<!--      <option value="Põlva maakond">Põlva maakond</option>-->
-<!--      <option value="Pärnu maakond">Pärnu maakond</option>-->
-<!--      <option value="Rapla maakond">Rapla maakond</option>-->
-<!--      <option value="Saare maakond">Saare maakond</option>-->
-<!--      <option value="Tartu maakond">Tartu maakond</option>-->
-<!--      <option value="Valga maakond">Valga maakond</option>-->
-<!--      <option value="Viljandi maakond">Viljandi maakond</option>-->
-<!--      <option value="Võru maakond">Võru maakond</option>-->
-<!--    </select>-->
-
-
-
-
+    <v-app id="inspire">
+      <v-main class="grey lighten-3">
+        <v-container>
+          <h1>Marketplace</h1>
+          <br>
+          <h2>Search for an advertisement</h2>
+          <br>
+          <h4>Category</h4>
+          <input v-model="category1" placeholder="Choose category"/>
+          <br>
+          <h4>Price</h4>
+          <input v-model="price1" placeholder="Price from"/>
+          <input v-model="price1" placeholder="Price to"/>
+          <br>
+          <h4>Location</h4>
+          <input v-model="location1" placeholder="Choose location"/>
+          <br>
+          <button v-on:click="getAllAdvertisementsButton()">Search</button>
+          {{ getAllAdvertisements }}
+          <br>
+          <h3>All Advertisement</h3>
+          <table>
+            <tr>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Location</th>
+            </tr>
+            <tr v-for="advertisement in advertisements">
+              <td>{{ advertisement.description }}</td>
+              <br>
+              <td>{{ advertisement.price }}</td>
+              <br>
+              <td>{{ advertisement.location }}</td>
+              <td>{{ advertisement }}</td>
+            </tr>
+          </table>
+          <button v-on:click="getAllAdvertisementsButton()">Search</button>
+        </v-container>
+      </v-main>
+    </v-app>
   </div>
 </template>
 
@@ -66,33 +50,26 @@ export default {
       'category1': '',
       'price1': '',
       'location1': '',
-
-      // 'getAds':'',
-      // 'Harju maakond': '',
-      // 'Hiiu maakond': ''
-
-
+      advertisements: []
     }
   },
+  methods: {
+    'getAllAdvertisementsButton': function () {
+      this.$http.put('/api/getAllAdvertisements/', {
+        advertisements: this.advertisements
+      })
+          .then(response => {
+            console.log(response);
+            this.advertisements = response.data
+          });
+    },
+  },
 
-
-  // methods: {
-  //
-  //   'getAdsByLocation': function (){
-  //     this.$http.get('/api/getAdsByLocation/' + this.location1)
-  //         .then(response => {
-  //           console.log(response);
-  //           this.getAds = response.data
-  //         })
-  //
-  //
-  //
-  //   }
-  // }
-
-
-
-
-
+  mounted() {
+    this.$http.get("/api/getAllAdvertisements/")
+        .then(response => this.advertisements = response.data);
+  }
 }
 </script>
+
+
