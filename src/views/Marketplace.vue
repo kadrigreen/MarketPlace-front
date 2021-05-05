a948165df95efaef9f1ef94daaf3e38a9b15f0dd
Kelly Miadzieles <kellymiadzieles@gmail.com>
2021-05-05 14:04:18 +0300

    commit_kelly

--- src/views/Marketplace.vue
+++ src/views/Marketplace.vue
@@ -40,24 +40,6 @@
         </v-container>
       </v-main>
     </v-app>
-    <h1>Marketplace</h1>
-    <br>
-    <h3>Serach for an advertisement</h3>
-    <br>
-    <h4>Category</h4>
-    <input v-model="category1" placeholder="Choose category"/>
-    <br>
-    <h4>Price</h4>
-    <input v-model="priceFrom" placeholder="Price from"/>
-    <input v-model="priceTo" placeholder="Price to"/>
-    <button v-on:click="getAdsByPrice">Insert</button>
-    {{priceResults}}
-    <br>
-    <h4>Location</h4>
-    <input v-model="location1" placeholder="Choose location"/>
-
-
-
   </div>
 </template>
 
@@ -68,12 +50,7 @@ export default {
       'category1': '',
       'price1': '',
       'location1': '',
-
       advertisements: []
-      'priceFrom': '',
-      'priceTo': '',
-      'priceResults': '',
-
     }
   },
   methods: {
@@ -91,17 +68,6 @@ export default {
   mounted() {
     this.$http.get("/api/getAllAdvertisements/")
         .then(response => this.advertisements = response.data);
-
-
-  methods: {
-    'getAdsByPrice': function () {
-      this.$http.get('/api/getAdsByPrice/' + this.priceFrom + "/" + this.priceTo)
-          .then(response => {
-            console.log(response);
-            this.priceResults = response.data;
-          })
-    }
-
   }
 }
 </script>
