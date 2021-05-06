9d93ffad3b6974d8bd612110ed38a14bf6a0bcdf
Kelly Miadzieles <kellymiadzieles@gmail.com>
2021-05-06 19:54:58 +0300

    commit_06.05

--- src/views/addAdvertisement.vue
+++ src/views/addAdvertisement.vue
@@ -1,52 +1,65 @@
 <template>
   <v-app id="inspire">
-    <v-app-bar
-        app
-        color="white"
-        flat
-    >
-      <v-avatar
-          :color="$vuetify.breakpoint.smAndDown ? 'grey darken-1' : 'transparent'"
-          size="32"
-      ></v-avatar>
-
-      <v-tabs
-          centered
-          class="ml-n9"
-          color="grey darken-1"
-      >
-        <v-tab
-            v-for="link in links"
-            :key="link"
-        >
-          {{ link}}
+    <v-app-bar app color="white" flat>
+      <v-tabs centered class="ml-9" color="blue lighten-2">
+        <v-tab v-for="link in links" :key="link">
+          {{ link }}
         </v-tab>
-
       </v-tabs>
-      <v-avatar
-          class="hidden-sm-and-down"
-          color="grey darken-1 shrink"
-          size="32"
-      ></v-avatar>
+      <v-avatar class="hidden-sm-and-down" color="blue lighten-2" size="32"></v-avatar>
     </v-app-bar>
-
     <v-main class="grey lighten-3">
       <v-container>
         <v-row>
-
-
-          <v-col
-              cols="12"
-              sm="9"
-          >
-            <v-sheet
-                min-height="80vh"
-                rounded="lg"
-            >
-              <!--  -->
+          <v-col cols="12" sm="14">
+            <v-sheet min-height="80vh" rounded="lg">
+              <h1>Create new advertisement</h1>
+              <br>
+              <h2>Click here to add a picture</h2>
+              <button v-on:click="addPictureButton()">Add picture</button>
+              <br>
+              <br>
+              <h4>Title</h4>
+              <input v-model="title" placeholder="Enter title"/>
+              <br>
+              <h4>Description</h4>
+              <input v-model="description" placeholder="Enter description"/>
+              <br>
+              <h4>Price</h4>
+              <input v-model="price" placeholder="Enter price"/>
+              <br>
+              <h4>Category</h4>
+              <v-select
+                  :items="items2"
+                  label="Please select category"
+                  v-model = 'selectedCategory'
+                  v-on:change="getAdsByCategory()">
+                Category
+              </v-select>
+              <br>
+              <h4>Location</h4>
+              <v-select
+                  :items="items"
+                  label="Please select location"
+                  v-model = 'chosenLocation'
+                  v-on:change="getAdsByLocation()">
+                Location
+              </v-select>
+              <h4>Name</h4>
+              <input v-model="userName" placeholder="Enter name"/>
+              <br>
+              <h4>Phone number</h4>
+              <input v-model="phoneNumber" placeholder="Enter number"/>
+              <br>
+              <h4>eMail</h4>
+              <input v-model="eMail" placeholder="Enter email"/>
+              <br>
+              <br>
+              <button v-on:click="createAdvertisementButton()">Create Advertisement</button>
+              {{ advertisement }}
+              <br>
             </v-sheet>
           </v-col>
-
         </v-row>
       </v-container>
     </v-main>
@@ -55,41 +68,69 @@
 
 <script>
 export default {
-  data: () =>
-      ({
-        items: ['Harju maakond', 'Hiiu maakond', 'Ida-Viru maakond', 'Jõgeva maakond', 'Järva maakond', 'Lääne maakond', 'Lääne-Viru maakond', 'Põlva maakond', 'Pärnu maakond', 'Rapla maakond', 'Saare maakond', 'Tartu maakond', 'Valga maakond', 'Viljandi maakond', 'Võru maakond'],
-        'locationResponse': [],
-        'selectedLocation': '',
-        links: [
-          'MarketPlace',
-          'Add new advertisement',
-          'Profile',
-          'Updates',
-        ],
-      }),
-  return: {
-    'priceFrom': '',
-    'priceTo': '',
-    'priceResults': '',
-    'resultsByPrice': [],
+  data: function () {
+    return {
+      'title': '',
+      'description': '',
+      'price': '',
+      'userName': '',
+      'phoneNumber': '',
+      'eMail': '',
+      'advertisement': '',
+      items: ['Harju county', 'Hiiu county', 'Ida-Viru county', 'Jõgeva county', 'Järva county', 'Lääne county', 'Lääne-Viru county', 'Põlva county', 'Pärnu county', 'Rapla county', 'Saare county', 'Tartu county', 'Valga county', 'Viljandi county', 'Võru county'],
+      'chosenLocationResponse': [],
+      'chosenLocation': '',
+      'selectedLocation': '',
+      items2: ['Cars', 'Electronics', 'Pets', 'Real estate', 'Clothing and shoes', 'Home', 'Books', 'Construction', 'Leisure', 'Products for children'],
+      'categoryResponse': [],
+      'selectedCategory': '',
+      links: [
+        'Marketplace',
+        'Add new advertisement',
+        'Information',
+        'Updates',
+      ],
+    }
   },
-
   methods: {
-    'getAdsByPrice': function () {
-      this.$http.get('/api/getAdsByPrice/' + this.priceFrom + "/" + this.priceTo)
+    /*    'addPictureButton': function () {
+          this.$http.post('/api/addPicture/' ,{
+
+              });
+        },*/
+    'createAdvertisementButton': function () {
+      this.$http.post('/api/createAdvertisement/', {
+            title: this.title,
+            description: this.description,
+            price: this.price,
+            category: this.selectedCategory,
+            location: this.chosenLocation,
+            username: this.userName,
+            phonenumber: this.phoneNumber,
+            email: this.eMail
+          }
+      )
           .then(response => {
             console.log(response);
-            this.resultsByPrice = response.data;
+            this.advertisement = response.data;
           })
     },
     'getAdsByLocation': function () {
-      this.$http.get('/api/getAdsByLocation/' + this.selectedLocation)
+      this.$http.get('/api/getAdsByLocation/' + this.chosenLocation)
           .then(response => {
             console.log(response);
-            this.locationResponse = response.data
+            this.chosenLocationResponse = response.data
+          })
+    },
+    'getAdsByCategory': function () {
+      this.$http.get('/api/getAdsByCategory/' + this.selectedCategory)
+          .then(response => {
+            console.log(response);
+            this.categoryResponse = response.data
           })
     }
-  },
+  }
 }
+
 </script>
 
